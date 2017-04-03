20237b79172c9fd5b9619de0a56b7dde00333630
hastalafiesta <mauroscianca98@gmail.com>
2017-04-03 18:34:41 +0200

    update
    - Celsius to kelvin
    - Fahreneit to kelvin
    - kelvin to fahreneit
    - code improvements
    - check which OS is running to select the appropriate clear screen
    method

--- c/converter/main.c
+++ c/converter/main.c
@@ -1,15 +1,30 @@
 #include <stdio.h>
 
+#ifdef _WIN32_WINNT
+#define clear() system("cls")
+#else
+#define clear() system("clear")
+#endif
+
 void menu();
 int menuSel();
+int backMenu();
+
 double celToFar(double *val, double *converted);
+double celToK(double *val, double *converted);
+
 double FarToCel(double *val, double *converted);
+double FarToK(double *val, double *converted);
+
+double kToCel(double *val, double *converted);
+double kToFar(double *val, double *converted);
 
 int main(int argc, char **argv)
 {
     
     double converted=0;
     double val=0;
+     start: 
     
 	printf("Converter\n");
     menu();
@@ -20,17 +35,51 @@ int main(int argc, char **argv)
         scanf("%lf", &val);
         
         printf("%f", celToFar(&val, &converted));
+        
+        if(backMenu() == 121){
+           clear();
+        goto start;
+        
+        }
         break;
         case 2:
         printf("Insert value: ");
         scanf("%lf", &val);
         
+        printf("%f",celToK(&val, &converted));
+        break;
+        case 3:
+        printf("Insert value: ");
+        scanf("%lf", &val);
+        
         printf("%f",FarToCel(&val, &converted));
+        break;
+        case 4:
+        printf("Insert value: ");
+        scanf("%lf", &val);
+        
+        printf("%f",FarToK(&val, &converted));
+        break;
+        
+        case 5:
+        printf("Insert value: ");
+        scanf("%lf", &val);
+        
+        printf("%f",kToCel(&val, &converted));
+        break;
+        case 6:
+        printf("Insert value: ");
+        scanf("%lf", &val);
+        
+        printf("%f",kToFar(&val, &converted));
         break;
     }
     
 	return 0;
 }
+
+
+// Celsius
 
 double celToFar(double *val, double *converted){
    
@@ -39,6 +88,15 @@ double celToFar(double *val, double *converted){
     return *converted;
 }
 
+double celToK(double *val, double *converted){
+    
+    *converted = *val + 273.15;
+
+    return *converted;
+}
+
+// Fahreneit
+
 double FarToCel(double *val, double *converted){
    
     *converted = (*val - 32) / 1.8;
@@ -46,11 +104,35 @@ double FarToCel(double *val, double *converted){
     return *converted;
 }
 
+double FarToK(double *val, double *converted){
+    
+    *converted = (*val + 459.67) / 1.8;
+
+    return *converted;
+}
+
+// Kelvin
+
+double kToCel(double *val, double *converted){
+    
+    *converted = *val - 273.15;
+
+    return *converted;
+}
+
+double kToFar(double *val, double *converted){
+    
+    *converted = (*val * 1.8) / 459.67;
+
+    return *converted;
+}
+
 void menu(){
     
-    char *c[] = { "1. Celsius to Farheneit", "2. Farheneit to Celsius"};
+    const char *c[] = { "1. Celsius to Farheneit", "2. Celsius to Kelvin", "3. Fahreneit to Celsius",
+                "4. Fahreneit to Kelvin", "5. Kelvin to Celsius", "6. Kelvin to Fahreneit"};
     
-    for (int i=0; i< 2; i++)
+    for (int i=0; i< 6; i++)
     printf("%s\n", c[i]);
     
 }
@@ -63,4 +145,15 @@ int menuSel(){
     
     return sel;
     
+}
+
+int backMenu(){
+    
+    char c;
+    
+    printf("\nBack to main menu? y/n");
+    printf("\nYour answer is: ");
+    scanf(" %c", &c);
+    
+    return c;
 }
